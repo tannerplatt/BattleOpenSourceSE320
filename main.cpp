fe628ce72b5ac3da9be1c76bb4616072f7415476
tannerplatt <tplatt@chapman.edu>
2024-04-07 16:25:30 -0700

    added unit test structure

--- main.cpp
+++ main.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
+#include <string>
 #include "battle.h"
 
-int main() {
-    std::string playerName;
-    std::cout << "Enter your character's name: ";
-    std::getline(std::cin, playerName);
 
-    //creating the player and a basic AI opponent
-    Player player(playerName, 100, 15); //player with 100 health and 15 attack power
-    Player ai("AI_Opponent", 100, 13); //AI opponent with 100 health and 13 attack power
+void unitTests(){
+}
 
-    //starting the battle
-    Battle battle(player, ai);
-    battle.startBattle();
+int main(int argc, char **argv) {
+    bool unitTestFlag = false;
+    for (int i = 1; i < argc; i++){
+        if(std::string(argv[i]) == "-test"){
+            unitTestFlag = true;
+            break;
+        }
+    }
 
+    if (unitTestFlag){
+        unitTests();
+    } else {
+        std::string playerName;
+        std::cout << "Enter your character's name: ";
+        std::getline(std::cin, playerName);
+
+        //creating the player and a basic AI opponent
+        Player player(playerName, 100, 15); //player with 100 health and 15 attack power
+        Player ai("AI_Opponent", 100, 13); //AI opponent with 100 health and 13 attack power
+
+        //starting the battle
+        Battle battle(player, ai);
+        battle.startBattle();
+    }
     return 0;
 }
